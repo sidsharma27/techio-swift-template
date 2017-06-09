f3687b5fb6c0ce3f5198c841a0d35186844bfd6b
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-06-08 21:50:43 -0700

    Initial Commit

--- swift-template/Sources/swift_template.swift
+++ swift-template/Sources/swift_template.swift
@@ -2,7 +2,7 @@ class swift_template {
     func countAllStars(galaxies: Int...) -> Int {
 	    var totalStars:Int = 0;
 	    for stars in galaxies {
-		    totalStars = stars; // fix me!
+		    totalStars = stars; // fix me! (Hint: +=)
 	    } 
 	    return totalStars;
     }
