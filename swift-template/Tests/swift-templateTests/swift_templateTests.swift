5bc72363fe45d3dea8af9de06155be13b2d51a9b
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-06-08 23:00:43 -0700

    Fixed swift file

--- swift-template/Tests/swift-templateTests/swift_templateTests.swift
+++ swift-template/Tests/swift-templateTests/swift_templateTests.swift
@@ -5,9 +5,7 @@ class swift_templateTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        if XCTAssertEqual(swift_template().countAllStars(galaxies: 2, 3), 5) {
-            print("true")
-        }
+        print(XCTAssertEqual(swift_template().countAllStars(galaxies: 2, 3), 5))
         XCTAssertEqual(swift_template().countAllStars(galaxies: 9, -3), 6)
     }
 
